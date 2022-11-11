5bc1dfc2ffe6bb50228278c35086f7625943a3b4
abhishek2285 <abhisheksingh6716@gmail.com>
2022-11-11 09:27:06 +0530

    strings problems

--- Revision/strings/strings.cpp
+++ Revision/strings/strings.cpp
@@ -31,11 +31,60 @@ void replaceChar(char str[], char s1, char s2){
     }
 }
 
+void reverseStr(char str[]){
+    int len = lengthOfStr(str);
+    int i=0, j=len-1;
+    while(i<j){
+        char temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        i++;
+        j--;
+    }
+}
 
+void trimSpaces(char str[]){
+    int i=0, j=0;
+    while(str[i] != '\n'){
+        if(str[i] == ' '){
+            i++;
+        } else{
+            str[j]=str[i];
+            i++;
+            j++;
+        }
+    }
+}
+
+void reverse(char input[], int start, int end){
+    int i=start, j=end;
+    while(i<j){
+        char temp=input[i];
+        input[i]=input[j];
+        input[j]=temp;
+        i++;
+        j--;
+    }
+}
+void reverseStringWordWise(char input[]){
+    int previousSpaceIndex=-1;
+    int i=0;
+    while(input[i] != '\0'){
+        if(input[i]==' '){
+            reverse(input, previousSpaceIndex+1, i-1);
+            previousSpaceIndex=i;
+        }
+        i++;
+    }
+    reverse(input, previousSpaceIndex+1, i-1);
+    reverse(input, 0, i-1);
+}
 
 int main(){
     char str[100];
-    cin>>str;
+    // cin>>str;
     // cout<<lengthOfStr(str);
-    cout<<isPalindrome(str);
+    cin.getline(str, 100);
+    trimSpaces(str);
+    cout<<str;
 }
