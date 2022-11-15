ac9936dba1d065ccb664d1590232d11172d68518
abhishek2285 <abhisheksingh6716@gmail.com>
2022-11-15 09:06:03 +0530

    spiral print

--- Revision/2DArrays/2DArrays.cpp
+++ Revision/2DArrays/2DArrays.cpp
@@ -1,7 +1,87 @@
 #include<iostream>
 using namespace std;
 
-void columnWiseSum(int **arr, int m, int n){
+void spiralPrint(int arr[][100], int m, int n){
+    int i, k = 0, l = 0;
+    /* k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator
+    */
+    while (k < m && l < n) {
+        for (i = l; i < n; ++i) {
+            cout << arr[k][i] << " ";
+        }
+        k++;
+ 
+        for (i = k; i < m; ++i) {
+            cout << arr[i][n - 1] << " ";
+        }
+        n--;
+ 
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << arr[m - 1][i] << " ";
+            }
+            m--;
+        }
+ 
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << arr[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
+
+void wavePrint(int arr[][100], int m, int n){
+    for(int j=0; j<n; j++){
+        if(j%2==0){
+            for(int i=0; i<m; i++){
+                cout<<arr[i][j]<<" ";
+            }
+        } else{
+            for(int i=m-1; i>=0; i--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void largestRowOrColumn(int arr[][100], int m, int n){
+    bool isrow = true;
+    int index, sum=0;
+    for(int i=0; i<m; i++){
+        int sum1=0;
+        for(int j=0; j<n; j++){
+            sum1+=arr[i][j];
+        }
+        if(sum1>sum){
+            sum=sum1;
+            index=i;
+            isrow=true;
+        }
+    }
+    for(int i=0; i<n; i++){
+        int sum2=0;
+        for(int j=0; j<m; j++){
+            sum2+=arr[j][i];
+        }
+        if(sum2>sum){
+            sum=sum2;
+            index=i;
+            isrow=false;
+        }
+    }
+    if(isrow){
+        cout<<"row "<<index<<" "<<sum<<endl;
+    }
+}
+
+void columnWiseSum(int arr[][100], int m, int n){
     for(int i=0; i<n; i++){
         int sum=0;
         for(int j=0; j<m; j++){
@@ -9,9 +89,10 @@ void columnWiseSum(int **arr, int m, int n){
         }
         cout<<sum<<" ";
     }
+    cout<<endl;
 }
 
-void printArrColmnWise(int **arr, int m, int n){
+void printArrColmnWise(int arr[][100], int m, int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             cout<<arr[j][i]<<" ";
@@ -20,7 +101,7 @@ void printArrColmnWise(int **arr, int m, int n){
     }
 }
 
-void printArrRowWise(int **arr, int m, int n){
+void printArrRowWise(int arr[][100], int m, int n){
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
             cout<<arr[i][j]<<" ";
@@ -42,26 +123,16 @@ int main(){
         }
     }
     cout<<endl;
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    printArrRowWise(arr, m, n);
     cout<<endl;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cout<<arr[j][i]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl<<"////////// column wise sum /////////////"<<endl;
-    for(int i=0; i<n; i++){
-        int sum=0;
-        for(int j=0; j<m; j++){
-            sum+=arr[j][i];
-        }
-        cout<<sum<<" ";
-    }
+    printArrColmnWise(arr, m, n);
+    cout<<endl;
+    columnWiseSum(arr, m, n);
+    cout<<endl;
+    largestRowOrColumn(arr, m ,n);
+    cout<<endl;
+    wavePrint(arr, m, n);
+    cout<<endl;
+    spiralPrint(arr, m, n);
     cout<<endl;
 }
