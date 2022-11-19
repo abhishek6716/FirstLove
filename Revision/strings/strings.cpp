a8f64f8cf858c50379e9daabe1d830529e51c707
abhishek2285 <abhisheksingh6716@gmail.com>
2022-11-19 13:06:56 +0530

    string problems

--- Revision/strings/strings.cpp
+++ Revision/strings/strings.cpp
@@ -117,13 +117,120 @@ void printAllSubstrings(char str[]){
     }
 }
 
+bool isPermutation(char str1[], char str2[]){
+    if(strlen(str1) != strlen(str2)){
+        return false;
+    }
+    int arr[256] = {0};
+    for(int i=0; i<strlen(str1); i++){
+        arr[int(str1[i])]++;
+    }
+    for(int i=0; i<strlen(str1); i++){
+        arr[int(str2[i])]--;
+    }
+    for(int i=0; i<256; i++){
+        if(arr[i] != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void removeConsecutiveDuplicates(char input[]){
+    int i=0, k=1;
+    while(k<=strlen(input)){
+        if(input[i] != input[k]){
+            input[i+1]=input[k];
+            i++;
+        } else{
+            k++;
+        }
+    }
+    cout<<input;
+}
+
+void reverseEachWord(char input[]){
+    int i=0;
+    for(int j=0; j<strlen(input); j++){
+        if(input[j] == ' ' || j==strlen(input)-1){
+            if(j==strlen(input)-1){
+                reverse(input, i, j);
+            } else{
+                reverse(input, i, j-1);
+                i=j+1;
+            }
+        }
+    }
+    cout<<input<<endl;
+}
+
+void removeX(char input[], char x){
+    int k=0, i=0;
+    while(input[i]!='\0'){
+        if(input[i]!=x){
+            input[k]=input[i];
+            i++;
+            k++;
+        } else{
+            i++;
+        }
+    }
+    input[k]='\0';
+    cout<<input<<endl;
+}
+
+char highestOccuringChar(char input[]){
+    int arr[256] = {0};
+    for(int i=0; i<strlen(input); i++){
+        arr[input[i]]++;
+    }
+    int max=arr[0];
+    int maxIndex=0;
+    for(int i=0; i<256; i++){
+        if(arr[i]>max){
+            max=arr[i];
+            maxIndex=i;
+        }
+    }
+    return char(maxIndex);
+}
+
+void stringCompression(char input[]){
+    int k=0, i=0;
+    while(i<strlen(input)){
+        char curr=input[i];
+        int cnt=0;
+        while(i<strlen(input) && input[i] == curr){
+            i++;
+            cnt++;
+        }
+        input[k]=curr;
+        k++;
+        if(cnt > 1) {
+            string count_str = to_string(cnt);
+            for(char &ch : count_str) {
+                input[k] = ch;
+                cnt++;
+            }
+        }
+    }
+    cout<<input<<endl;
+}
+
 int main(){
-    char str[100];
+    char str1[100], str2[100];
     // cin>>str;
     // cout<<lengthOfStr(str);
-    cin.getline(str, 100);
+    cin.getline(str1, 100);
+    // cin.getline(str2, 100);
     // trimSpaces(str);
     // cout<<str;
     // printAllPrefixes(str);
-    printAllSubstrings(str);
+    // printAllSubstrings(str);
+    // cout<<isPermutation(str1, str2);
+    // removeConsecutiveDuplicates(str1);
+    // reverseEachWord(str1);
+    // removeX(str1, 'b');
+    // cout<<highestOccuringChar(str1)<<endl;
+    stringCompression(str1);
 }
